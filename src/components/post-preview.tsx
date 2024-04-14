992e1f7ea06cff4d030c2d3b5719508ccbe73b7b
Ben Ilegbodu <ben@benmvp.com>
2024-04-14 18:20:05 -0500

    Fix links of images to include render mode

--- src/components/post-preview.tsx
+++ src/components/post-preview.tsx
@@ -27,7 +27,12 @@ export function PostPreview({
   return (
     <div>
       <div className="mb-5">
-        <CoverImage slug={slug} title={title} src={coverImage} />
+        <CoverImage
+          slug={slug}
+          title={title}
+          src={coverImage}
+          renderMode={renderMode}
+        />
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
         <Link href={`/posts-${renderMode}/${slug}`} className="hover:underline">
